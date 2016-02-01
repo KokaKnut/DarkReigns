74e670d6ca3166b6bda4477907bc3da690668c94
KokaKnut <mwehof@yahoo.com>
2016-02-01 12:56:40 -0500

    created project
    added a few assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float runSpeed;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        
+    }
+
+    public void Move(float h, float v, bool jump)
+    {
+        if (jump)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 1);
+
+        rb.velocity = new Vector2(runSpeed * h, rb.velocity.y);
+
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (h < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+    private PlayerController player;
+	
+    // Use this for initialization
+	void Awake () {
+        player = GetComponent<PlayerController>();
+	}
+	
+    void Update()
+    {
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        player.Move(0f, 0f, jump);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = 0f;
+        h = (Input.GetKey(KeyCode.A) ? h - 1 : h);
+        h = (Input.GetKey(KeyCode.D) ? h + 1 : h);
+
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        player.Move(h, 0f, jump);
+    }
+}
