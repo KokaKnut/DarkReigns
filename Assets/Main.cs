8921c7bfc59ab841d50027a58b6752063eff89f0
KokaKnut <mwehof@yahoo.com>
2016-07-08 13:05:26 -0400

    added ui and a demo

---
+++ Assets/Main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+	public void Reload()
+    {
+        SceneManager.LoadScene("workspace");
+    }
+}
