735dfbd0e73caa35ba7e44dc6c56f69a82e4a679
Marcus Wehof <mwehof@yahoo.com>
2016-08-15 16:05:28 -0400

    framed out some shuffler code

--- Assets/Scripts/World Generation/WeightedShuffler.cs
+++ Assets/Scripts/World Generation/WeightedShuffler.cs
@@ -24,17 +24,49 @@ public class WeightedItem
     }
 }
 
-public class WeightedShuffler<T> {
+public class WeightedShuffler {
 
-    private List<T> items;
+    private List<WeightedItem> items;
+    private float weightSum;
+    private float weightMin;
 
 	//constructor
     public WeightedShuffler()
     {
-        items = new List<T>();
+        items = new List<WeightedItem>();
+        weightSum = 0;
+        weightMin = float.MaxValue;
     }
 
     //adds an item to current list with its probabillity
+    public void Add(object o, float weight)
+    {
+        items.Add(new WeightedItem(o, weight, weightSum, weightSum + weight));
+
+        if (weightMin > weight)
+            weightMin = weight;
+
+        weightSum += weight;
+    }
     
     //shuffle the list
+    public List<object> GetShufledList()
+    {
+        List<object> list = new List<object>();
+
+        //copy the original list so we can remove items without losing them
+        List<WeightedItem> itemsP = new List<WeightedItem>();
+        foreach (WeightedItem item in items)
+        {
+            itemsP.Add(item);
+        }
+
+        //loop until empty
+        //get random number in range
+        //search down items looking at range for appropiate hit
+        //if range is missing, linear probe for new hit
+        //
+
+        return list;
+    }
 }
